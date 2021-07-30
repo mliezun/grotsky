469b906397af90346f01aa6ee7bb89318a52b1ae
Miguel Liezun <liezun.js@gmail.com>
2021-07-30 15:28:04 -0300

    Remove HTTP globals

--- internal/grotskyGlobals.go
+++ internal/grotskyGlobals.go
@@ -3,7 +3,6 @@ package internal
 import (
 	"errors"
 	"io/ioutil"
-	"net/http"
 	"os"
 	"path/filepath"
 )
@@ -58,7 +57,6 @@ func (n *nativeObj) String() string {
 
 func defineGlobals(state *interpreterState, e *env, p IPrinter) {
 	defineIo(e, p)
-	defineHTTP(state, e)
 	defineType(e)
 	defineImport(state, e)
 	defineEnv(e)
@@ -212,117 +210,6 @@ func defineIo(e *env, p IPrinter) {
 		methods: map[string]*nativeFn{
 			"println":  &println,
 			"readfile": &readfile,
-		},
-	})
-}
-
-func defineHTTP(state *interpreterState, e *env) {
-	var handler nativeFn
-	handler.callFn = func(arguments []interface{}) (interface{}, error) {
-		if len(arguments) != 2 {
-			return nil, errInvalidNumberArguments
-		}
-		pattern, isString := arguments[0].(grotskyString)
-		if !isString {
-			return nil, errExpectedString
-		}
-		handle, isFunction := arguments[1].(grotskyCallable)
-		if !isFunction {
-			return nil, errExpectedFunction
-		}
-		http.HandleFunc(string(pattern), func(w http.ResponseWriter, req *http.Request) {
-			responseWriter := &nativeObj{
-				methods: map[string]*nativeFn{
-					"write": {
-						callFn: func(arguments []interface{}) (interface{}, error) {
-							if len(arguments) != 2 {
-								return nil, errInvalidNumberArguments
-							}
-							code, ok := arguments[0].(grotskyNumber)
-							if !ok {
-								return nil, errExpectedNumber
-							}
-							exec.mx.Unlock()
-							defer exec.mx.Lock()
-							w.WriteHeader(int(code))
-							body := arguments[1].(grotskyString)
-							if _, err := w.Write([]byte(body)); err != nil {
-								return nil, err
-							}
-							return nil, nil
-						},
-					},
-				},
-			}
-
-			grotskyHeader := make(grotskyDict)
-			for header, vals := range req.Header {
-				grotskyVals := make(grotskyList, len(vals))
-				for i := range vals {
-					grotskyVals[i] = grotskyString(vals[i])
-				}
-				grotskyHeader[grotskyString(header)] = grotskyVals
-			}
-
-			requestReader := &nativeObj{
-				properties: map[string]interface{}{
-					"method":    grotskyString(req.Method),
-					"address":   grotskyString(req.RemoteAddr),
-					"userAgent": grotskyString(req.UserAgent()),
-					"host":      grotskyString(req.Host),
-					"uri":       grotskyString(req.RequestURI),
-					"referer":   grotskyString(req.Referer()),
-					"headers":   grotskyHeader,
-				},
-				methods: map[string]*nativeFn{
-					"body": {
-						callFn: func(arguments []interface{}) (interface{}, error) {
-							if len(arguments) != 0 {
-								return nil, errInvalidNumberArguments
-							}
-							exec.mx.Unlock()
-							defer exec.mx.Lock()
-							rqBody, err := ioutil.ReadAll(req.Body)
-							if err != nil {
-								return nil, err
-							}
-							return grotskyString(rqBody), nil
-						},
-					},
-				},
-			}
-
-			exec.mx.Lock()
-			defer exec.mx.Unlock()
-			_, err := handle.call([]interface{}{
-				requestReader,
-				responseWriter,
-			})
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				state.logger.Println(err)
-			}
-		})
-		return nil, nil
-	}
-
-	var listen nativeFn
-	listen.callFn = func(arguments []interface{}) (interface{}, error) {
-		if len(arguments) != 1 {
-			return nil, errInvalidNumberArguments
-		}
-		if addr, ok := arguments[0].(grotskyString); ok {
-			exec.mx.Unlock()
-			defer exec.mx.Lock()
-			return nil, http.ListenAndServe(string(addr), nil)
-		}
-		return nil, errExpectedString
-	}
-
-	e.define("http", &nativeObj{
-		methods: map[string]*nativeFn{
-			"handler": &handler,
-			"listen":  &listen,
 		},
 	})
 }

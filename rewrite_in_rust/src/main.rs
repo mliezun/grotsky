bb8dfc70f429145f9b1c3431368c7ba76532d3ce
Miguel Liezun <liezun.js@gmail.com>
2023-06-06 15:57:20 -0600

    Read file from source in rust

--- rewrite_in_rust/src/main.rs
+++ rewrite_in_rust/src/main.rs
@@ -1,5 +1,7 @@
 mod lexer;
 
+use std::env;
+use std::fs::read_to_string;
 use std::time::Instant;
 
 const SOURCE: &str = "
@@ -10,8 +12,17 @@ while a < 10000000 {
 ";
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let content: String;
+    let source: &str;
+    if args.len() > 1 {
+        content = read_to_string(&args[1]).unwrap();
+        source = content.as_str();
+    } else {
+        source = SOURCE;
+    }
     let start = Instant::now();
-    lexer::scan(String::from(SOURCE));
+    lexer::scan(String::from(source));
     let duration = start.elapsed();
     println!("{:?}", duration.as_secs_f64());
 }

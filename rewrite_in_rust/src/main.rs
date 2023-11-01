0ac8ac8908a7c30946e0eaf1bb4003f6894916b9
Miguel Liezun <liezun.js@gmail.com>
2023-11-01 20:02:59 -0300

    Validate cli args to grotsky rs

--- rewrite_in_rust/src/main.rs
+++ rewrite_in_rust/src/main.rs
@@ -16,31 +16,20 @@ use std::fs::{canonicalize, read_to_string};
 
 use std::{env, panic};
 
-const SOURCE: &str = "
-let a = 1
-while a < 1000000 {
-    a = a + 1
-}
-";
-
-fn string_to_static_str(s: String) -> &'static str {
-    Box::leak(s.into_boxed_str())
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage:\n\tgrotsky [filename]");
+        return;
+    }
     let content: String;
     let source: &str;
     let abs_path = canonicalize(&args[1]).unwrap();
     let abs_path_str = abs_path.to_string_lossy();
     let abs_path_string = String::from(abs_path_str);
     interpreter::set_absolute_path(abs_path_string);
-    if args.len() > 1 {
-        content = read_to_string(interpreter::get_absolute_path()).unwrap();
-        source = content.as_str();
-    } else {
-        source = SOURCE;
-    }
+    content = read_to_string(interpreter::get_absolute_path()).unwrap();
+    source = content.as_str();
     let grotsky_debug = env::var("GROTSKY_DEBUG").unwrap_or("0".to_string());
     if grotsky_debug != "1" && !grotsky_debug.eq_ignore_ascii_case("true") {
         // Disable rust backtrace
